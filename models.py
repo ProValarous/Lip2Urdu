657d143e3754222140269beb5a4c193adc1abf60
Radhika Khatri <74490667+radhikakhatrii@users.noreply.github.com>
2023-06-03 16:35:19 +0500

    Update models.py

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import math
+import numpy as np
 
 def conv3x3(in_planes, out_planes, strd=1, padding=1, bias=False):
     "3x3 convolution with padding"
